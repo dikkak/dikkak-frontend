8469c4b9e0daa1014ab09f234e0417a74c2a0fe9
sangminlee98 <tkdals0975@naver.com>
2022-05-10 01:22:41 +0900

    [이메일회원가입1,2] 타임라인

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
 import FindAccount from "./pages/FindAccount";
+import EmailSignUp1 from './pages/EmailSignUp1';
+import EmailSignUp2 from './pages/EmailSignUp2';
 const Router = () => {
   return (
     <BrowserRouter>
@@ -10,6 +12,8 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/findAccount" element={<FindAccount />} />
+        <Route path="/email_signup1" element={<EmailSignUp1 />} />
+        <Route path="/email_signup2" element={<EmailSignUp2 />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/EmailSignUp2.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import Menu from '../components/Menu';
+
+const Container = styled.div`
+  max-width: 1440px;
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  margin-top: 82px;
+`;
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  width: 70%;
+  height: 660px;
+  padding: 40px 0;
+`;
+const BackButton = styled.div`
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 150px;
+  padding: 5px 10px;
+  background-color: #717171;
+  border-radius: 5px;
+
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+  p {
+    font-size: 0.8rem;
+    color: white;
+  }
+`;
+
+const TimeLine = styled.div`
+  margin: 20px auto;
+  padding: 20px;
+  height: 50vh;
+`;
+const Outer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 52%;
+  border-left: 1px solid ${props => props.theme.mainColor};
+  h3 {
+    color: ${props => props.theme.subColor};
+    &::before {
+      background: ${props => props.theme.mainColor};
+      border: 3px solid ${props => props.theme.mainColor};
+    }
+    &:first-child:not(.not) {
+      color: ${props => props.theme.mainColor};
+    }
+  };
+`;
+const Outer2 = styled(Outer)`
+  height: 48%;
+  justify-content: end;
+  border-left: 1px solid #C4C4C4;
+  h3 {
+    color: #C4C4C4;  
+    &::before {
+      background: #C4C4C4;
+      border: 3px solid #C4C4C4;
+    }
+  }
+`;
+
+const Title = styled.h3`
+  color: black;
+  position: relative;
+  margin: 0 0 0 30px;
+  &::before {
+    content: "";
+  position: absolute;
+  width: 10px;
+  height: 10px;
+  border-radius: 999px;
+  left: -38.5px;
+  }
+`;
+
+const EmailSignUp2 = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Menu/>
+      <Container>
+        <Wrapper>
+          <BackButton onClick={() => navigate(-1)}>
+            <p>◀︎</p>
+            <p>이전으로 돌아가기</p>
+          </BackButton>
+          <TimeLine>
+            <Outer>
+              <Title>계정생성</Title> 
+              <Title>약관동의</Title> 
+            </Outer>
+            <Outer2>
+              <Title className='not'>가입완료</Title>
+            </Outer2>
+        </TimeLine>
+        </Wrapper>
+      </Container>
+    </>
+  );
+};
+
+export default EmailSignUp2;
