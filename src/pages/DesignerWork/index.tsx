09741a0b6f42c1359af1fbf48ed3a1fc82ca6137
sangminlee98 <tkdals0975@naver.com>
2022-06-23 17:09:07 +0900

    [designer 작업실1] 1차완료 & 더미데이터 형식 수정

--- src/pages/DesignerWork/index.tsx
+++ src/pages/DesignerWork/index.tsx
@@ -1,24 +1,25 @@
-import React from "react";
+import React from 'react';
 import Menu from "../../components/Menu";
 import Footer from "../../components/Footer";
 import Document from "../../components/Document";
 import { useNavigate } from "react-router-dom";
 import { BackButton, Container, DocumentContainer, LogoImage, Title, Wrapper } from './styles';
 
-const clientContent = {
-  title: "제안서",
+
+const completeWork = {
+  title: "완료된 작업",
   contents: [
     {
       id: 1,
-      name: '20220418 디깍 로고제작',
+      name: '스파르타 코리안 ZEP / 스파르타코리안',
     },
     {
       id: 2,
-      name: '로고 제작2',
+      name: '상세페이지 / 캠퍼스소싱',
     },
     {
       id: 3,
-      name: '포스터 제작(3/8)',
+      name: '홍보 포스터 제작 / 플랜잇',
     },
   ],
   workMenttion: "제안서 작업실",
@@ -30,22 +31,19 @@ const companyContent = {
   contents: [
     {
       id: 1,
-      name: '디깍 로고 제작 / 000디자이너 / 2차 작업중',
+      name: '디깍 로고 제작 / 클라이언트명 / 2차 작업중',
     },
     {
       id: 2,
-      name: '디깍 로고 제작 / 000디자이너 / 1차 작업중',
-    },
-    {
-      id: 3,
-      name: '포스터 제작',
+      name: 'SNS굿 로고 제작 / 클라이언트명 / 작업 완료',
     },
   ],
   workMenttion: "외주 작업실",
   bgColor: "#329A29",
 };
 
-const ClientWorkPage = () => {
+const DesignerWorkPage = () => {
+  
   const navigate = useNavigate();
   return (
     <>
@@ -58,13 +56,13 @@ const ClientWorkPage = () => {
           </BackButton>
           <Title>
             <div>
-              <h1>클라이언트 작업실</h1>
+              <h1>디자이너 작업실</h1>
               <LogoImage></LogoImage>
             </div>
-            <p>외주작업을 위한 000 클라이언트 작업실 입니다</p>
+            <p>외주작업을 위한 000 디자이너 작업실 입니다</p>
           </Title>
           <DocumentContainer>
-            <Document content={clientContent}></Document>
+            <Document content={completeWork}></Document>
             <Document content={companyContent}></Document>
           </DocumentContainer>
         </Wrapper>
@@ -74,4 +72,4 @@ const ClientWorkPage = () => {
   );
 };
 
-export default ClientWorkPage;
+export default DesignerWorkPage;
