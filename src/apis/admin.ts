e3bfc4e6a2e3898691dd63aed907910bf6a3c548
sangminlee98 <tkdals0975@naver.com>
2022-09-21 17:51:42 +0900

    [어드민 페이지] 전체 제안서 조회 & 외주작업실 메뉴

--- src/apis/admin.ts
+++ src/apis/admin.ts
@@ -1,4 +1,12 @@
 import axios from "axios";
+
+export const getAllProposal = async (page: number = 0) => {
+  const response = await axios.get<ProposalList>(
+    `/admin/proposal/list?page=${page}`
+  );
+  return response.data;
+};
+
 export const getProposalByEmail = async (email: string) => {
   const data = { email };
   const response = await axios.post<IClientProposal[]>(
@@ -23,4 +31,19 @@ export interface IClientProposal {
   id: number;
   title: string;
   createdAt: string;
+}
+export interface ProposalList {
+  totalPages: number;
+  totalCount: number;
+  page: number;
+  size: number;
+  contents: Content[];
+}
+
+export interface Content {
+  proposalId: number;
+  email: string;
+  nickname: string;
+  title: string;
+  createdAt: Date;
 }
---
+++ src/pages/OutsourcePage/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import OutsourceMenu from "../../components/OutsourceMenu";
+
+const OutsourcePage = () => {
+  return (
+    <div>
+      <OutsourceMenu />
+    </div>
+  );
+};
+
+export default OutsourcePage;
